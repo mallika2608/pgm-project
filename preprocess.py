9c6f2a525ec8f9d94eea5eee9fa3b97aac52b6e6
kshanmol <kshasingh@gmail.com>
2017-04-26 03:15:33 +0530

    Modularize trivial shit because boredom

--- preprocess.py
+++ preprocess.py
@@ -19,7 +19,7 @@ def readDataFile():
 
 	return my_data
 
-#Removse sets of observations in which the value of atleast one variable is an outlier
+#Removes sets of observations in which the value of atleast one variable is an outlier
 #Outliers defined as values lying further than 3*stddev from mean 
 def remove_outliers(my_data):
 
@@ -42,7 +42,7 @@ def remove_outliers(my_data):
 
 	return new_data
 
-def get_quantile(item, first_boundary, second_boundary):
+def get_value(item, first_boundary, second_boundary):
 
 	if(item <= first_boundary):
 		return 0
@@ -51,44 +51,31 @@ def get_quantile(item, first_boundary, second_boundary):
 	else:
 		return 2
 
-def quantile_discretize(my_data):
+#Discretize the protein level values
+#Modes supported - interval and quantile discretization
+def discretize(my_data, mode = 'quantile'):
 
 	new_data = []
 
 	for row in my_data:
-		first_boundary = np.percentile(row, 33.33)
-		second_boundary = np.percentile(row, 66.66)
 
-		new_row = [ get_quantile(item, first_boundary, second_boundary) for item in row ]
-		print new_row.count(0), new_row.count(1), new_row.count(2)
-		new_data.append(new_row)
+		if (mode == 'quantile'):
+			first_boundary = np.percentile(row, 33.33)
+			second_boundary = np.percentile(row, 66.66)
 
-	new_data = np.array(new_data)
-	#print new_data
+		elif (mode == 'interval'):
+			row_min, row_max= np.min(row), np.max(row)
+			interval_length = (row_max - row_min)/3.0
 
-	return new_data
+			first_boundary = row_min + interval_length
+			second_boundary = row_min + 2*interval_length
 
+		else:
+			print 'Invalid mode for discretize function'
+			sys.exit(1)
 
-def get_interval(item, interval_length, row_min, row_max):
-
-	if(item <= row_min + interval_length):
-		return 0
-	if(item <= row_min + 2*interval_length):
-		return 1
-	else:
-		return 2
-
-def interval_discretize(my_data):
-
-	new_data = []
-
-	for row in my_data:
-		row_min = np.min(row)
-		row_max = np.max(row)
-
-		interval_length = (row_max - row_min)/3.0
-		new_row = [ get_interval(item, interval_length, row_min, row_max) for item in row ]
-		print new_row.count(0), new_row.count(1), new_row.count(2)
+		new_row = [ get_value(item, first_boundary, second_boundary) for item in row ]
+		#print new_row.count(0), new_row.count(1), new_row.count(2)
 		new_data.append(new_row)
 
 	new_data = np.array(new_data)
@@ -100,5 +87,5 @@ if __name__ == '__main__':
 	my_data = readDataFile()
 	my_data = remove_outliers(my_data)
 	#my_data = interval_discretize(my_data)
-	my_data = quantile_discretize(my_data)
+	my_data = discretize(my_data, 'quantile')
 	np.savetxt("data/2_quantile_discretized.csv", my_data.T, delimiter = ",", fmt = '%d')
